71c0be5efb8c4af70b9e801e28abbae3ef89fed6
Paul Lee <Paul.Lee@teknowlogi.com>
2020-06-15 22:02:32 -0700

    Adds react bootstrap dependency and adds modal component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import TaskForm from "./components/taskForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { show: false };
+
+  handleOpen = () => {
+    let { show } = this.state;
+    show = true;
+    this.setState({ show });
+    console.log("after", this.state);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <main className="container">
+          <Button onClick={this.handleOpen}>Open Modal</Button>
+          <TaskForm show={this.state.show} />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/taskForm.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+const TaskForm = ({ show }) => {
+  return (
+    <Modal show={show} dialogClassName="modal-30w">
+      <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary">Close</Button>
+        <Button variant="primary">Save Changes</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default TaskForm;
